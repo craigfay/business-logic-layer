26488cc3cae104863b2a0fd01847a727db5d8a9d
Craig Fay <craigfay1@gmail.com>
2019-09-05 15:21:12 -0500

    Use AcmeInventory object

--- service-providers/inventory-manager.netsuite.ts
+++ service-providers/inventory-manager.netsuite.ts
@@ -2,22 +2,20 @@ import { InventoryManagerContract } from '../contracts/inventory-manager'
 import { Product } from '../entities'
 import { ClientError, ServerError } from '../error-types'
 
-class Netsuite implements InventoryManagerContract {
+const AcmeInventory: InventoryManagerContract = {
   async verifyInStock(products:Product[]) {
     return products
-  }
+  },
   async countInventory(productId:number) {
     return 100
-  }
+  },
   async updateInventoryQuantity(productId:number, newQuantity:number) {
     return new ClientError('The product id provided does not exist')
-  }
+  },
   async increaseInventoryQuantity(productId:number, amount:number) {
     return new ServerError('Our system took to long to respond')
-  }
+  },
   async decreaseInventoryQuantity(productId:number, amount:number) {
     return 100 - amount
-  }
+  },
 }
-
-export const InventoryManager = new Netsuite();

6fdfb7d08e5c47b510252678b4c6293791e7e17d
Craig Fay <craigfay1@gmail.com>
2019-09-05 08:52:48 -0500

    Rename items -> products

--- offerings/one-time-purchase.ts
+++ offerings/one-time-purchase.ts
@@ -1,13 +1,13 @@
 export async function oneTimePurchase(options) {
-  const { items, price, paymentData, customerData} = options;
+  const { products, price, paymentData, customerData} = options;
 
-  let inStockReport = await checkInventory(items)
+  let inStockReport = await checkInventory(products)
   if (inStockReport instanceof Error) return inStockReport
 
   let paymentProcessingReport = await processPayment(price, paymentData)
   if (paymentProcessingReport instanceof Error) return paymentProcessingReport 
 
-  let inTransitReport = await initiateDelivery(items, customerData)
+  let inTransitReport = await initiateDelivery(products, customerData)
   if (inTransitReport instanceof Error) return inTransitReport 
 
   return { success: true }

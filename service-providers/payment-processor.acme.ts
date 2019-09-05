baa0b5cfac9ee94e0a6301caada954cd06f1dd35
Craig Fay <craigfay1@gmail.com>
2019-09-05 15:45:03 -0500

    AcmePayments.refundPayment()

--- service-providers/payment-processor.acme.ts
+++ service-providers/payment-processor.acme.ts
@@ -8,29 +8,58 @@ import {
 } from '../entities'
 
 const AcmePayments: PaymentProcessorContract = {
-  processPayment: async (payment:PaymentDetails) => {
-    let dummyPaymentReceipt = {
-      id: 'dn8fbvm139hd0hc1',
-      timestamp: new Date().toUTCString(),
-      cents: 5000,
-      products: [
-        {
-          id: 'c9gh385nckdhadf0',
-          name: 'Olive Tee',
-          slug: 'olive-tee',
-          cents: 1600,
-          description: 'A tee shirt you can bring home to mom and dad.',
-          tags: ['shirts', 'wholesome'],
-          meta: {}
-        }
-      ],
-      metafields: {}
-    }
-    return dummyPaymentReceipt;
-  }
-  // refundPayment: (receipt:PaymentReceipt) => Promise<RefundReceipt | AcceptableError>
+  processPayment,
+  refundPayment,
   // createSubscription: (payment:PaymentDetails, subscription:SubscriptionDetails) => Promise<PaymentReceipt | AcceptableError>
   // cancelSubscription: (subscriptionId:string) => Promise<SubscriptionDetails | AcceptableError>
+}
+
+/**
+ * Generate and return a dummy PaymentReceipt
+ */
+async function processPayment(payment:PaymentDetails) {
+  let dummyReceipt = {
+    id: 'dn8fbvm139hd0hc1',
+    timestamp: new Date().toUTCString(),
+    cents: 5000,
+    products: [
+      {
+        id: 'c9gh385nckdhadf0',
+        name: 'Olive Tee',
+        slug: 'olive-tee',
+        cents: 1600,
+        description: 'A tee shirt you can bring home to mom and dad.',
+        tags: ['shirts', 'wholesome'],
+        meta: {}
+      }
+    ],
+    metafields: {}
+  }
+  return dummyReceipt;
+}
+
+/**
+ * Generate and return a dummy RefundReceipt
+ */
+async function refundPayment(receipt:PaymentReceipt) {
+  let dummyRefund = {
+    id: '72hd9cn38fh3ga0o',
+    timestamp: new Date().toUTCString(),
+    cents: 20000,
+    products: [
+      {
+        id: 'c9gh385nckdhadf0',
+        name: 'Olive Tee',
+        slug: 'olive-tee',
+        cents: 1600,
+        description: 'A tee shirt you can bring home to mom and dad.',
+        tags: ['shirts', 'wholesome'],
+        meta: {}
+      }
+    ],
+    meta: {}
+  }
+  return dummyRefund;
 }
 
 export default AcmeInventory;

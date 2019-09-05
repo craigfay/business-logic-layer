7af0e5d168a4b2e65eb03e105ee19a4c71bd577e
Craig Fay <craigfay1@gmail.com>
2019-09-05 14:21:13 -0500

    WIP fill out oneTimePurchase()

--- entities/product.ts
+++ entities/product.ts
@@ -1,8 +1,9 @@
 export interface Product {
+  id: number
   name: string
   slug: string
   price: number
   description: string
-  tags: string[]
+  tags: Array<string>
   metafields: { [key:string]: string }
 }
--- offerings/one-time-purchase.ts
+++ offerings/one-time-purchase.ts
@@ -1,15 +1,46 @@
-import { InventoryManager } from '../service-providers/inventory-manager.netsuite'
+import { Product } from '../entities/product'
 
-export async function oneTimePurchase(options) {
-  const { products, price, paymentData, customerData} = options;
+import { InventoryManager } from '../service-providers/inventory-manager.shopify'
+
+interface OneTimePurchaseOptions {
+  products:Product[]
+  cents:number
+  payment:PaymentDetails
+  shipping:ShippingDetails
+}
+
+interface PaymentDetails {
+  cardNumber:string
+  expirationMonth:number
+  expirationYear:number
+  securityCode:number
+  firstName:string
+  lastName:string
+  streetAddress:string
+  stateProvince:string
+  country:string
+  metadata: { [key:string]:string }
+}
+
+interface ShippingDetails {
+  firstName:string
+  lastName:string
+  streetAddress:string
+  stateProvince:string
+  country:string
+  metadata: { [key:string]:string }
+}
+
+export async function oneTimePurchase(options:OneTimePurchaseOptions) {
+  const { products, cents, payment, shipping } = options;
 
   let inStockReport = await InventoryManager.verifyInStock(products)
   if (inStockReport instanceof Error) return inStockReport
 
-  let paymentProcessingReport = await PaymentProcessor.processPayment(price, paymentData)
+  let paymentProcessingReport = await PaymentProcessor.processPayment(cents, payment)
   if (paymentProcessingReport instanceof Error) return paymentProcessingReport 
 
-  let inTransitReport = await ProductDelivery.initiateDelivery(products, customerData)
+  let inTransitReport = await ProductDelivery.initiateDelivery(products, shipping)
   if (inTransitReport instanceof Error) return inTransitReport 
 
   return { success: true }

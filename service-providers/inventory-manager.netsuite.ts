d8a1d8ac2f11cb12aa62e5d98aeb97c4117b4b9c
Craig Fay <craigfay1@gmail.com>
2019-09-05 08:50:42 -0500

    Service Providers

---
+++ service-providers/inventory-manager.netsuite.ts
@@ -0,0 +1,21 @@
+import { InventoryManagerContract } from '../contracts/inventory-manager'
+import { Product } from '../entities/product'
+import { ClientError, ServerError } from '../error-types'
+
+export class Netsuite implements InventoryManagerContract {
+  async verifyProductsInStock(products:Product[]) {
+    return products
+  }
+  async countInventory(productId:number) {
+    return 100
+  }
+  async updateInventoryQuantity(productId:number, newQuantity:number) {
+    return new ClientError('The product id provided does not exist')
+  }
+  async increaseInventoryQuantity(productId:number, amount:number) {
+    return new ServerError('Our system took to long to respond')
+  }
+  async decreaseInventoryQuantity(productId:number, amount:number) {
+    return 100 - amount
+  }
+}
